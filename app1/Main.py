6090065c67c9961121c0d2d4066ca8974c4870a0
Негрик-на-польях <3609653@gmail.com>
2026-01-29 22:27:29 +0300

    а

--- app1/Main.py
+++ app1/Main.py
@@ -4,22 +4,22 @@ from telebot import types
 TOKEN = '8587792984:AAG0k70U93Pl-W72pleShz8QMt6T4gnFnPU'
 bot = telebot.TeleBot(TOKEN)
 
-APP_URL = "https://negrik-na-polyah.github.io/TG-botik/?v=6"
+# Не забывай менять ?v=... чтобы обновлялось!
+APP_URL = "https://negrik-na-polyah.github.io/TG-botik/?v=clicker1"
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    
+    # ВАЖНО: открываем только через KeyboardButton
     web_app = types.WebAppInfo(url=APP_URL)
-    
-    btn = types.KeyboardButton(text="Запустить приложение 🚀", web_app=web_app)
+    btn = types.KeyboardButton(text="Играть в Кликер 🎮", web_app=web_app)
     markup.add(btn)
-    
-    bot.send_message(message.chat.id, "Нажми на кнопку ниже, чтобы открыть Mini App:", reply_markup=markup)
-    
+    bot.send_message(message.chat.id, "Нажми на кнопку ниже, чтобы начать игру:", reply_markup=markup)
+
 @bot.message_handler(content_types=['web_app_data'])
-def handle_app_data(message):
-    print(f"Получены данные: {message.web_app_data.data}") # Это появится в терминале VS Code
-    bot.send_message(message.chat.id, f"Ура! Бот получил: {message.web_app_data.data}")
+def handle_data(message):
+    # Получаем наше число кликов
+    score = message.web_app_data.data
+    bot.send_message(message.chat.id, f"🏆 Твой результат сохранен! Ты накликал: {score}")
 
 bot.infinity_polling()
